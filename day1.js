4b982447cbfd18bfa3ac5ad13487056250a01a90
Shannon Bertucci <hello@shannon.io>
2021-12-10 00:24:03 -0700

    Complete Day1

--- day1.js
+++ day1.js
@@ -1,23 +1,9 @@
 import data from './day1input.js';
 
-// const data = [
-//     199,
-//     200,
-//     208,
-//     210,
-//     200,
-//     207,
-//     240,
-//     269,
-//     260,
-//     263
-// ];
-
-const run = () => {
+const first = (data) => {
     const final = data.reduce((count, curr, index) => {
         if (curr > data[index-1]) {
             count = count + 1;
-            console.log(curr, count);
         }
 
         return count;
@@ -26,4 +12,47 @@ const run = () => {
     return final;
 }
 
-console.log(run());
+// const data = [
+//     607,
+//     618,
+//     618,
+//     617,
+//     647,
+//     716,
+//     769,
+//     792
+// ];
+
+const second = (data) => {
+    let firstWindow = [0,1,2];
+    let secondWindow = [1,2,3];
+
+    let currIndex = 2;
+    let count = 0;
+
+    while (currIndex < data.length - 1) {
+        // console.log(firstWindow, secondWindow);
+        const sumA = data[firstWindow[0]] + data[firstWindow[1]] + data[firstWindow[2]];
+        const sumB = data[secondWindow[0]] + data[secondWindow[1]] + data[secondWindow[2]];
+        // console.log(sumB)
+        if (sumB > sumA) {
+            count = count + 1;
+        }
+
+        firstWindow = secondWindow;
+        secondWindow = secondWindow.map(n => n+1);
+        
+        currIndex++;
+    }
+
+    return count;
+}
+
+const run = () => {
+    // console.log(first(data));
+    console.log(second(data));
+
+}
+
+run();
+
